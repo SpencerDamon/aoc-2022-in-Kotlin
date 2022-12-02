98284ad9f6203789fa93cede23fdc8280d0846e5
SpencerDamon <spencerdamon1974@gmail.com>
2022-12-01 19:12:28 -0800

    initial template setup

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,15 +3,17 @@ fun main() {
         return input.size
     }
 
+    /*
     fun part2(input: List<String>): Int {
         return input.size
     }
+     */
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+    // println(part2(input))
 }
