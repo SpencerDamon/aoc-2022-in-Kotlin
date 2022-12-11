bcb97f0d493f33ac975116e6366747d3ba8f252d
SpencerDamon <spencerdamon1974@gmail.com>
2022-12-10 20:29:16 -0800

    Day 1 Part 2 test complete inspired by Xeno Tactic

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,6 @@
 import kotlin.math.max
 fun main() {
+    /* Return highest sum dilineated by "" */
     fun part1(input: List<String>): Int {
         var highestSum = 0
         var currentSum = 0
@@ -16,18 +17,33 @@ fun main() {
         return highestSum
     }
 
-    /*
+    /* Return sum of highest 3 sums dilineated by "" */
     fun part2(input: List<String>): Int {
-        return input.size
+        val highestScores = mutableListOf<Int>() // store all the scores
+        var currentSum = 0
+        for (s in input) {
+            if (s.isEmpty()) {
+                highestScores.add(currentSum) // add current sum to list
+                currentSum = 0
+            } else {
+                currentSum += s.toInt() // add int value to var
+            }
+        }
+        highestScores.sortDescending() // sort list
+        //println
+        return highestScores.take(3).sum() // sums the numbers from the 3 highest scores
+        //return input.size
     }
-     */
 
+
+    // remove comment to test,
     // test if implementation meets criteria from the description, like:
-    //val testInput = File("src/Day01_test.txt").readText()
-    //check(part1(testInput) == 54000)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 24000)
+    //check(part2(testInput) == 41000)
 
 
-    val testInput = readInput("Day01_test")
-    println(part1(testInput))
-    // println(part2(input))
+    val input = readInput("Day01_input")
+    println(part1(input))
+    println(part2(input))
 }
