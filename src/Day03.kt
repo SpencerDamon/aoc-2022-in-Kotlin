659a9e9ae4687bd633bddfb924416c80a8a2db2e
SpencerDamon <spencerdamon1974@gmail.com>
2022-12-26 11:54:48 -0800

    Day 3 Part 2 complete

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,7 +2,7 @@
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    /*fun part1(input: List<String>): Int {
         var subString1 = ""
         var subString2 = ""
         val points = mutableListOf<Int>()
@@ -22,29 +22,43 @@ fun main() {
                     //println(points)
                     break
                 }
-
             }
-
-
         }
 
-        //println(points.sum())
         return points.sum()
-    }
-
-/*    fun part2(input: List<String>): Int {
-
-        return 0
     }*/
+
+    fun part2(input: List<String>): Int {
+        val sums = mutableListOf<Int>()
+        for (i in 0 until input.size step 3) {
+            val line1 = input[i]
+            val line2 = input[i + 1]
+            val line3 = input[i + 2]
+
+           for (j in 0 until line1.length) {
+
+                if (line2.contains(line1[j]) && line3.contains(line1[j])) {
+                    sums.add(when (line1[j]) {
+                        in 'a'..'z' -> line1[j].code - 96
+                        else -> line1[j].code - 38
+                    }
+                    )
+                    break
+                }
+            }
+        }
+
+        return sums.sum()
+    }
 
     // remove comment to test,
     // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day03_test")
-//    check(part1(testInput) == 157)
-    //check(part2(testInput) == 12)
+    //val testInput = readInput("Day03_pt2_test")
+    //check(part1(testInput) == 157)
+    //check(part2(testInput) == 70)
 
 
     val input = readInput("Day03_input")
-    println(part1(input))
-    //println(part2(input))
+//    println(part1(input))
+    println(part2(input))
 }
